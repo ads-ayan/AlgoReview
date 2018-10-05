1b4af6bcacfea8ea4e601a6240158a4990560f7a
ads-ayan <3146413+ads-ayan@users.noreply.github.com>
2018-10-05 16:46:44 +0800

    Update hashtable.cpp

--- hashtable.cpp
+++ hashtable.cpp
@@ -1,13 +1,13 @@
-// HashTable.cpp : Defines the entry point for the console application.
+// Coding_Exer_HashMap.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 #include <malloc.h>
+#define MAX 50
 
 typedef struct keypair {
 	unsigned char *key;
 	int value;
-	int in_use;
 	int dib;
 	unsigned long hash;
 } keypair;
@@ -15,19 +15,17 @@ typedef struct keypair {
 typedef struct hashmap {
 	int table_size;
 	int size;
-	keypair *data[1000];
+	keypair *data[MAX];
 } hashmap;
 
-int strlen(unsigned char * s) {
+int strlen(unsigned char *s) {
 	unsigned char *p = s;
 	for (; *p != '\0'; p++) {}
 	return p - s;
 }
 
-int strCmp(unsigned char* s1, unsigned char* s2)
-{
-	while (*s1 == *s2)
-	{
+int strCmp(unsigned char *s1, unsigned char *s2) {
+	while (*s1 == *s2) {
 		if (*s1 == '\0') return(0);
 		s1++;
 		s2++;
@@ -35,7 +33,7 @@ int strCmp(unsigned char* s1, unsigned char* s2)
 	return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
-unsigned long hash(unsigned char *str, int len) {
+unsigned long hash_function(unsigned char *str) {
 	unsigned long hash = 5381;
 	int c;
 
@@ -46,16 +44,16 @@ unsigned long hash(unsigned char *str, int len) {
 	return hash;
 }
 
-unsigned long hash_key(hashmap *h, unsigned char *str, int len) {
-	unsigned long key = hash(str, len);
-	return key % h->table_size;
+unsigned long hash_key(hashmap *h, unsigned char *str) {
+	unsigned long key = hash_function(str);
+	return key%h->table_size;
 }
 
 hashmap *hashmap_new(int size) {
 	hashmap *h;
 	h = (hashmap*)malloc(sizeof(hashmap));
 
-	for (int i = 0; i < 1000; i++) {
+	for (int i = 0; i < MAX; i++) {
 		h->data[i] = NULL;
 	}
 
@@ -64,171 +62,128 @@ hashmap *hashmap_new(int size) {
 	return h;
 }
 
-/*
-
-int hashmap_resize(hashmap *h) {
-	int new_size = h->table_size;
-	int factor = 2;
-
-	while (new_size <= (factor * h->size)) {
-		new_size *= 2;
-	}
-	if (new_size == h->table_size) {
-		return 0;
-	}
-
-	hashmap *nd = hashmap_new(new_size);
-}
-
-*/
-
 int hashmap_add(hashmap *h, unsigned char *key, int val) {
+	if (h->table_size == h->size) {
+		return -1; //full
+	}
 	if (!h || !key) {
 		return -1;
 	}
-
-	unsigned long hash = hash_key(h, key, strlen(key));
-	unsigned long hashvalue = hash;
-	int dib = 0;
-	while (1) {
-		if (h->data[hash] == NULL || h->data[hash]->in_use != 1) {
-			if (h->data[hash] == NULL) {
-				h->data[hash] = (keypair*)malloc(sizeof(keypair));
-			}
-			h->data[hash]->key = key;
-			h->data[hash]->value = val;
-			h->data[hash]->in_use = 1;
-			h->data[hash]->dib = dib;
-			h->data[hash]->hash = hashvalue;
+	unsigned long hash = hash_function(key);
+	unsigned long index_init = hash % h->table_size;
+	int dib_current = 0;
+	unsigned long index_current = index_init;
+	int i = 0;
+	for (i = 0; i < h->table_size; i++) {
+		index_current = (index_init + i) % h->table_size;
+		if (h->data[index_current] == NULL) {
+			h->data[index_current] = (keypair*)malloc(sizeof(keypair));
+			h->data[index_current]->key = key;
+			h->data[index_current]->value = val;
+			h->data[index_current]->dib = dib_current;
+			h->data[index_current]->hash = hash;
 			h->size++;
 			break;
 		}
-		if (h->data[hash]->dib < dib) {
-			int loc_val = h->data[hash]->value;
-			unsigned char *loc_key = h->data[hash]->key;
-
-			h->data[hash]->key = key;
-			h->data[hash]->value = val;
-			h->data[hash]->dib = dib;
-			h->data[hash]->hash = hashvalue;
-			h->data[hash]->in_use = 1;
-			hashmap_add(h, loc_key, loc_val);
-			free(loc_key);
+		else if (h->data[index_current]->hash == hash && !strCmp(key, h->data[index_current]->key)) {
+			h->data[index_current]->value = val;
 			break;
 		}
-		dib++;
-		hash = hash + 1;
-	}
+		else {
+			if (h->data[index_current]->dib < dib_current) {
+				int loc_val = h->data[index_current]->value;
+				unsigned char *loc_key = h->data[index_current]->key;
+				unsigned long loc_hash = h->data[index_current]->hash;
+				unsigned long loc_dib = h->data[index_current]->dib;
 
-	if (3 * h->size > 2 * h->table_size) {
-		//resize
+				h->data[index_current]->key = key;
+				h->data[index_current]->value = val;
+				h->data[index_current]->hash = hash;
+				h->data[index_current]->dib = dib_current;
+
+				dib_current = loc_dib;
+				key = loc_key;
+				val = loc_val;
+				hash = loc_hash;
+			}
+		}
+		dib_current++;
 	}
 
 	return 0;
 }
+
 int hashmap_get_int(hashmap *h, unsigned char *key) {
-	unsigned long hash = hash_key(h, key, strlen(key));
+	unsigned long hash = hash_function(key);
+	unsigned long index_init = hash % h->table_size;
 	int found = -1;
-	while (1) {
-		if (h->data[hash]->key == NULL || h->data[hash]->in_use != 1) {
+	int i = 0;
+	for (i = 0; i < h->table_size; i++) {
+		unsigned long index_current = (index_init + i) % h->table_size;
+		if (h->data[index_current] == NULL) {
 			break;
 		}
-		if (h->data[hash]->hash == hash && !strCmp(key, h->data[hash]->key)) {
-			found = hash;
+		if (i > h->data[index_current]->dib) {
 			break;
 		}
-
-		hash = hash + 1;
-
+		if (h->data[index_current]->hash == hash && !strCmp(key, h->data[index_current]->key)) {
+			found = index_current;
+			break;
+		}
 	}
-
+	
 	return found;
 }
 
-int hashmap_get(hashmap *h, unsigned char *key) {
-	unsigned long hash = hash_key(h, key, strlen(key));
-	unsigned long hashvalue = hash;
-	int found = -1;
-	while (1) {
-		if (h->data[hash] == NULL || h->data[hash]->in_use != 1) {
-			break;
-		}
-		if (h->data[hash]->hash == hashvalue && !strCmp(key, h->data[hash]->key)) {
-			found = hash;
-			break;
-		}
-
-		hash = hash + 1;
-
-	}
-	if (found != -1) {
-		return h->data[hash]->value;
-	}
-	else {
-		return found;
-	}
-	
-}
-
 int hashmap_del(hashmap *h, unsigned char *key) {
-	int pos = hashmap_get_int(h, key);
-
-	if (pos < 0) {
-		return 0;
+	int index_current = hashmap_get_int(h, key);
+	if (index_current < 0) {
+		return -1;
 	}
 
-	if (h->data[pos]->in_use) {
-		if (strCmp(h->data[pos]->key, key) == 0) {
-			h->data[pos]->in_use = 0;
-			h->data[pos]->value = 0;
-			h->data[pos]->key = NULL;
+	free(h->data[index_current]);
+	h->data[index_current] = NULL;
 
-			h->size--;
-		}
-	}
-
-	int npos;
-
-	while (1) {
-		npos = pos + 1;
-		if (h->data[npos] == NULL || h->data[npos]->in_use != 1) {
+	int index_previous = 0, index_swap = 0;
+	int i = 1;
+	for (i = 1; i < h->table_size; i++) {
+		index_previous = (index_current + i - 1) % h->table_size;
+		index_swap = (index_current + i) % h->table_size;
+		if (h->data[index_swap] == NULL) {
+			free(h->data[index_previous]);
+			h->data[index_previous] = NULL;
 			break;
 		}
 
-		if (h->data[npos]->dib == 0) {
+		if (h->data[index_swap]->dib == 0) {
+			free(h->data[index_previous]);
+			h->data[index_previous] = NULL;
 			break;
 		}
 
-		h->data[pos]->key = h->data[npos]->key;
-		h->data[pos]->value = h->data[npos]->value;
-		h->data[pos]->hash = h->data[npos]->hash;
-		h->data[pos]->dib = h->data[npos]->dib - 1;
-		h->data[pos]->in_use = 1;
-
-		h->data[npos]->key = NULL;
-		h->data[npos]->value = 0;
-		h->data[npos]->in_use = 0;
-
-		pos = npos;
+		h->data[index_previous]->key = h->data[index_swap]->key;
+		h->data[index_previous]->value = h->data[index_swap]->value;
+		h->data[index_previous]->hash = h->data[index_swap]->hash;
+		h->data[index_previous]->dib = h->data[index_swap]->dib;
 	}
-
-	return -1;
+	return 0;
 }
-
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	unsigned char one[4] = { 'o', 'n', 'e', '\0' };
-	unsigned char twentytwo[] = { 't', 'w', 'e', 'n', 't', 'y', '-', 't', 'w', 'o','\0' };
+	unsigned char twentytwo[] = { 't', 'w', 'e', 'n', 't', 'y', '-', 't', 'w', 'o', '\0' };
 	unsigned char thirteen[] = { 't', 'h', 'i', 'r', 't', 'e', 'e', 'n', '\0' };
-	hashmap *a = hashmap_new(10);
+	hashmap *a = hashmap_new(50);
 	hashmap_add(a, &(one[0]), 1);
 	hashmap_add(a, &twentytwo[0], 22);
 	hashmap_add(a, &thirteen[0], 13);
-	hashmap_get(a, twentytwo);
+	int ans = hashmap_get_int(a, twentytwo);
 	hashmap_del(a, one);
-	hashmap_get(a, one);
-	hashmap_get(a, twentytwo);
+	ans = hashmap_get_int(a, one);
+	ans = hashmap_get_int(a, twentytwo);
 	return 0;
 }
+
+
 
